cda48653be615153f0b2c6ee39481372e4322d65
inesmariao <inesmao.misena@gmail.com>
2024-08-15 07:14:13 -0500

    configure cors, bodyParser and /ruta-prueba

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import connection from "./database/connection.js";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 // Mensaje de bienvenida para verificar que ejecutó bien la API de Node
 console.log("API Node en ejecución");
@@ -11,7 +13,28 @@ connection();
 const app = express();
 const puerto = process.env.PORT || 3900;
 
-// Otras configuraciones
+// Configurar cors para hacer las peticiones correctamente
+app.use(cors({
+  origin: '*',
+  methods: 'GET,HEAD, PUT,PATCH,POST,DELETE'
+}));
+
+// Decodificar los datos desde los formularios para convertirlos en objetos JS
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Configurar rutas del aplicativo
+
+// Ruta de prueba
+app.get('/ruta-prueba', (req, res) => {
+  return res.status(200).json(
+    {
+      'id': 1,
+      'name': 'Inés María',
+      'username': 'inesmaoh'
+    }
+  );
+});
 
 
 
